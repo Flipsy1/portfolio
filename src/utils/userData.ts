6f7363a59010022d6139f199ce0e9d5af1251c28
Flipsy1 <felipevie@hotmail.com>
2023-05-04 16:11:01 -0400

    update

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Felipe Nogueira",
+  githubUser: "Flipsy1",
+  linkedinUser: "felipe-nogueira-vieira",
+  whatsappNumber: "+5592991710880",
+  emailUser: "felipevie@hotmail.com",
+  instagramUser: "felipnv",
+  // facebookUser: "",
 };
